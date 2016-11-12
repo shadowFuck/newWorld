282066b045225fe57e838eb282c903a6851cd559
zhengxiaodong <961371955@qq.com>
2016-11-12 10:58:39 +0800

    creat hello world

---
+++ Hello Git/Hello Git/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  Hello Git
+//
+//  Created by qianfeng1 on 16/11/12.
+//  Copyright © 2016年 qianfeng1. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        print("Hello,GitHub!")
+    }
+
+   
+}
+
